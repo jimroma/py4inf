eb2b9c5c90d6c1346e1e8605a3285d434454b423
Jim <jim@jimroma.com>
2011-08-15 15:11:07 -0400

    ch02

---
+++ ch02/chap02a.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+name = raw_input("Enter your name: \n")
+print "Hello", name
+
+hours = raw_input("Enter hours: \n")
+rate = raw_input("Enter your rate: \n")
+x = (float(hours) * float(rate))
+print "Pay:", x
+
+
+cel = raw_input("Enter temp in celsius to convert to fahrenheit: \n")
+far = (int(cel) * 1.8) + 32
+print cel, "degrees Celsius is ", far, "degrees Fahrenheit"
