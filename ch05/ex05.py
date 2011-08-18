9488dec27fe84bd28e6ff24cdb6dfacc712988cd
Jim <jim@jimroma.com>
2011-08-17 22:59:56 -0400

    ch04

--- ch05/ex05.py
+++ ch05/ex05.py
@@ -1,14 +1,24 @@
 #!/usr/bin/python
 
 numlist = []
+total = 0
 while True:
     num = raw_input('Enter a number: ')
     print num
     numlist.append(num)
     if num == 'done':
+        numlist.pop()
         break
         
 print numlist
+#print min(numlist)
+#print max(numlist)
+
+for i in numlist:
+    total = total + int(i)
+print 'Sum:', total
+
+
 
     
 
