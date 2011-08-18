0101b1b3916da85a7c2fe41737dfd85a1c807318
Jim <jim@jimroma.com>
2011-08-17 22:02:24 -0400

    ch05

---
+++ ch05/ex05.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+numlist = []
+while True:
+    num = raw_input('Enter a number: ')
+    print num
+    numlist.append(num)
+    if num == 'done':
+        break
+        
+print numlist
+
+    
+
