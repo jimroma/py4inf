21d239349bc5f01ba73797994a4092400e9abb1f
Jim <jim@jimroma.com>
2011-08-27 18:14:22 -0400

    ch10

---
+++ ch10/ch10.1.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+
+import string
+fhand = open('mbox-short.txt')
+counts = dict()
+
+'''
+for line in fhand:
+    if 'From ' in line:
+        words = line.split()
+        word = words[1]
+    if word not in counts:
+        counts[word] = 1
+    else:
+        counts[word] += 1
+'''        
+for line in fhand:
+	if 'From ' in line:
+		#print line
+		words = line.split()
+		word = words[1]
+		#print word
+		if word not in counts:
+			counts[word] = 1
+		else:
+			counts[word] += 1
+        
+#print counts
+        
+lst = list()
+for key, val in counts.items():
+	lst.append( (val, key) )
+	
+lst.sort(reverse=True)
+
+for key, val in lst[:1]:
+	print val, key
+	
+#print lst
+
+
+
---
+++ ch14/ch10.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+import sqlite3
+ 
+conn = sqlite3.connect('music.db')
+cur = conn.cursor()
+
+cur.execute('DROP TABLE IF EXISTS Tracks ')
+cur.execute('CREATE TABLE Tracks (title TEXT, plays INTEGER)')
+
+conn.close()
+
+
