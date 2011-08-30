a00e018ba56c827705741e15d544a79ac2fbfcdc
Jim <jim@jimroma.com>
2011-08-30 13:15:16 -0400

    ch10

--- ch10/ch10.3.py
+++ ch10/ch10.3.py
@@ -1,23 +1,34 @@
 #!/usr/bin/python
 
 def histogram(L):
-    d = {}
-    for x in L:
-        if x in d:
-            d[x] += 1
-        else:
-            d[x] = 1
-    return d
+	d = dict()
+	for c in L :
+		d[c] = d.get(c, 0) + 1 #dictionary method get takes key as default value, if the key exists the default value is returned outerwise 0 is returned
+	return d
+	
 L = 'abracadabra'    
 d = histogram(L)
 
-hist = histogram(L)
-print hist
+def listItems(d):
+	lst = list()  #create a list that we'll put the dictionary values into
+	for key, val in d.items(): #run a for loop over the counts dictionary
+		lst.append ( (val, key) ) #creage a list of tuples
+	lst.sort(reverse=True) #sort it in reverse so the largest number is listed first
 
-lst = list()
-for key, val in d.items():
-    lst.append( (val, key)) 
-            
-for key, val in lst:
-    print val, key
+	for key, val in lst[:1] : # print the first item in the list
+		return val, key
 
+x = listItems(d)
+print x
+
+'''
+# Sort the dictionary by value
+lst = list()  #create a list that we'll put the dictionary values into
+for key, val in d.items(): #run a for loop over the counts dictionary
+	lst.append ( (val, key) ) #creage a list of tuples
+lst.sort(reverse=True) #sort it in reverse so the largest number is listed first
+
+for key, val in lst[:1] : # print the first item in the list
+	print val, key
+'''
+	
