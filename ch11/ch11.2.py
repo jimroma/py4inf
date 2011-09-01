3f6c947976e4baee15fa3ec0465ba196093b935a
Jim <jim@jimroma.com>
2011-09-01 12:33:31 -0400

    ch11

--- ch11/ch11.2.py
+++ ch11/ch11.2.py
@@ -3,7 +3,10 @@
 import re
 
 hand = open('mbox.txt')
-
+c = 0
+nums = list()
+num = 0
+avg = 0
 
 for line in hand:
 	line = line.rstrip()
@@ -11,11 +14,15 @@ for line in hand:
 	x = re.findall('^New Revision: ([0-9]*)', line)
 
 	if len(x) > 0 : 
-		#print line
-		print x
+		c += 1
+		num = float(x[0])
+		nums.append(num)
 		
-	
-		
+print 'Number of items:', c	
+total = sum(nums)
+print 'Sum of items:', total
+avg = total/c
+print 'Average:', avg
 
 
 
