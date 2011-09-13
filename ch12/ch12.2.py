86960f0a81eb85a9b0575ede5147a6a05ea88971
Jim <jim@jimroma.com>
2011-09-13 11:31:24 -0400

    ch12

--- ch12/ch12.2.py
+++ ch12/ch12.2.py
@@ -1,21 +1,14 @@
 #!/usr/bin/python
+
 import urllib
 
-x = 0
-counts = dict()
-fhand = urllib.urlopen('http://www.py4inf.com/code/romeo.txt')
-for line in fhand:
-	words = line.split()
-	for c in words:
-		x = x + 1
-print x
-print line
-
-
-
-'''
-
-	for word in words:
-		counts[word] = counts.get(word, 0) + 1
-print counts
-'''
+try:
+	name = raw_input('Enter URL: ')
+	fhand = urllib.urlopen(name)
+	s = fhand.read()
+	print s[:3000]
+	print 'Document length:',len(s)
+except:
+	print 'BAD URL'
+	
+	
